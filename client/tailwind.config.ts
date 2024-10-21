0ad3928f3b3b1fd85e027e25b083c45680a05cd2
신지윤 <ranji12@naver.com>
2024-10-21 19:17:36 +0900

    tailwind top 23rem추가

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -12,6 +12,9 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      spacing: {
+        '90': '23rem',
+      },
     },
   },
   plugins: [],
