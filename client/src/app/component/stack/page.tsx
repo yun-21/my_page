08d69530c6dd896cd76bf5d611200dfc3e11d1f2
신지윤 <ranji12@naver.com>
2024-10-23 18:47:38 +0900

    stack 나열

--- client/src/app/component/stack/page.tsx
+++ client/src/app/component/stack/page.tsx
@@ -4,19 +4,40 @@ import StackCss from "../stack_text/stack_css";
 export default function StackComponent() {
   return (
     <>
-      <TitleCss title="STACK"/>
+      <TitleCss title="STACK" />
       <div className="bg-blue-700 w-11/12 h-5/6 flex flex-col justify-evenly">
-        <div className="flex h-32 items-center">
-          <StackCss name="FRONT"/>
-          <div>이미지들</div>
+        <div className="flex h-32 items-center gap-10">
+          <StackCss name="FRONT" />
+          <div>HTML</div>
+          <div>JavaScript</div>
+          <div>TypeScript</div>
+          <div>React</div>
+          <div>Next.js</div>
+          <div>CSS</div>
+          <div>Vanilla Extract</div>
+          <div>TailwindCSS</div>
         </div>
-        <div className="flex h-32 items-center">
-          <StackCss name="BACK"/>
-          <div>이미지들</div>
+        <div className="flex h-32 items-center gap-10">
+          <StackCss name="BACK" />
+          <div>Node.js</div>
+          <div>Nest.js</div>
+          <div>Express</div>
+          <div>Python</div>
         </div>
-        <div className="flex h-32 items-center">
-          <StackCss name="DATABASE"/>
-          <div>이미지들</div>
+        <div className="flex h-32 items-center gap-10">
+          <StackCss name="DATABASE" />
+          <div>SQLite</div>
+          <div>MongoDB</div>
+          <div>PostgreSQL</div>
+          <div>MySQL</div>
+          <div>MariaDB</div>
+        </div>
+        <div className="flex h-32 items-center gap-10">
+          <StackCss name="Collaboration & Tools"/>
+          <div>Slack</div>
+          <div>Notion</div>
+          <div>Figma</div>
+          <div>AWS</div>
         </div>
       </div>
     </>
